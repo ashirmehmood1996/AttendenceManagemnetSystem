226bd28d219cee5cb46a1d07ba87af813478f926
Ashir <ashirmehmood1996@gmail.com>
2019-05-23 20:43:07 +0500

    initial commit

---
+++ app/src/main/java/com/android/example/attendencemanagemnetsystem/MainActivity.java
@@ -0,0 +1,87 @@
+package com.android.example.attendencemanagemnetsystem;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_LOGIN = 1001;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //set action bar
+    }
+
+    @Override
+    protected void onStart() {
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+            sendToLogin();
+        }
+        super.onStart();
+    }
+
+    private void sendToLogin() {
+        List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.PhoneBuilder().build());
+        startActivityForResult(AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers).build(), RC_LOGIN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == RC_LOGIN) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Welcome user ", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "login un successfull", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.nav_main_logout) {
+            if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+                AuthUI.getInstance()
+                        .signOut(this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(MainActivity.this, "logout successfull", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            } else {
+                Toast.makeText(this, "already logged out", Toast.LENGTH_SHORT).show();
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
